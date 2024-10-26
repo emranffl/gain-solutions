10258e6146f2031b07cb4d4283324caad8a58903
Emran Hossain <emranffl.biz@gmail.com>
2024-10-26 21:43:55 +0600

    refactor: Update database seeder command in vercel-seed.js

--- vercel-seed.js
+++ vercel-seed.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process")
 
 try {
   console.log("Running the database seeder file...")
-  execSync("pnpm run db-seed", { stdio: "inherit" })
+  execSync('ts-node --compiler-options \'{"module":"CommonJS"}\' prisma/seed.ts', { stdio: "inherit" })
 } catch (error) {
   console.error(error)
   process.exit(1)
