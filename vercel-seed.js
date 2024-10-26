a8ff3a5f6d2597095e3a00f6305483702f2cbafd
Emran Hossain <emranffl.biz@gmail.com>
2024-10-26 21:54:38 +0600

    remove vercel seed files

--- vercel-seed.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require("child_process")
-
-try {
-  console.log("Running the database seeder file...")
-  execSync('ts-node --compiler-options \'{"module":"CommonJS"}\' prisma/seed.ts', { stdio: "inherit" })
-} catch (error) {
-  console.error(error)
-  process.exit(1)
-}
