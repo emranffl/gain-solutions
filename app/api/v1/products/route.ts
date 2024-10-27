c8ff51429d7eed78da9acb19c42071620df4301e
Emran Hossain <emranffl.biz@gmail.com>
2024-10-27 18:06:08 +0600

    Refactor product search query in products route

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -1,3 +1,4 @@
+import { Prisma, ProductCategory } from "@prisma/client"
 import { prisma } from "@prisma/prisma-instance"
 import { paginationHandler } from "@utils/pagination-handler"
 import { responseHandler } from "@utils/response-handler"
@@ -10,8 +11,42 @@ export async function GET(req: NextRequest) {
     const { skip, limit, page } = paginationHandler(req)
     const sort = searchParams.get("sort") || "createdAt"
     const order = searchParams.get("order") || "desc"
-    const query = {
+    const searchQuery = searchParams.get("q")
+    const minPrice = searchParams.get("minPrice")
+    const min = minPrice ? Number(minPrice) : undefined
+    const maxPrice = searchParams.get("maxPrice")
+    const max = maxPrice ? Number(maxPrice) : undefined
+    const price = searchParams.get("price")
+    const query: Prisma.ProductWhereInput = {
       deletedAt: null, // Exclude soft-deleted products
+      name: {
+        contains: searchQuery || undefined, // Filter products by query term
+      },
+      description: {
+        contains: searchQuery || undefined, // Filter products by query term
+      },
+      category: {
+        equals: (searchParams.get("category") as ProductCategory) || undefined,
+      },
+      price:
+        min && max // Filter products by price range
+          ? {
+              gte: min,
+              lte: max,
+            }
+          : min // Filter products by minimum price
+            ? {
+                gte: min,
+              }
+            : max // Filter products by maximum price
+              ? {
+                  lte: max,
+                }
+              : price // Filter products by exact price
+                ? {
+                    equals: Number(price),
+                  }
+                : undefined,
     }
 
     // Retrieve products with pagination
